a207a9cea9cfb4420f3b257a0708e152d6d4ce47
home <prasantaroutray8@gmail.com>
2020-02-07 18:07:09 +0530

    committing for git my new project sabare

---
+++ src/main/java/com/inogain/sabre/Sabarerental.java
@@ -0,0 +1,12 @@
+package com.inogain.sabre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sabarerental {
+		public static void main(String[] args) {
+			SpringApplication.run(Sabarerental.class, args);
+		}
+
+}
